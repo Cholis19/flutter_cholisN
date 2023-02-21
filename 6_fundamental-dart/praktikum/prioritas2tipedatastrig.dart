9fd9fc9d61440615b2dac6085018a8e4a69b7cd0
Cholis19 <cholissanctis10@gmail.com>
2023-02-21 11:33:46 +0700

    first commit

---
+++ 6_fundamental-dart/praktikum/prioritas2tipedatastrig.dart
@@ -0,0 +1,14 @@
+void main() {
+  //cara 1
+  // String nama = 'Nur Holis';
+  // String jurusan = 'informatika';
+  // String kelas = 'Flutter';
+
+  //cara 2
+  String nama, jurusan, kelas;
+
+  nama = 'Nur Holis';
+  jurusan = 'informatika';
+  kelas = 'Flutter';
+  print('nama = $nama \njurusan : $jurusan \nkelas : $kelas');
+}
