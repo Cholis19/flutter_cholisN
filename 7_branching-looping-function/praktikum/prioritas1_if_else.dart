c436955bbf0cb51d7c960382d42c69c558b95d1f
Cholis19 <cholissanctis10@gmail.com>
2023-02-22 09:50:40 +0700

    first commit

---
+++ 7_branching-looping-function/praktikum/prioritas1_if_else.dart
@@ -0,0 +1,15 @@
+void main() {
+  //deklarasikan variabel
+  var nilai = 80;
+
+  //if statement
+  if (nilai > 70) {
+    print('nilai A');
+  } else if (nilai > 40 && nilai < 70) {
+    print('nilai B');
+  } else if (nilai > 0 && nilai < 40) {
+    print('nilai c');
+  } else {
+    print('teks kosong');
+  }
+}
