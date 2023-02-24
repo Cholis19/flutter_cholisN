1130a7c5d5027feaf6d31532e170f1ec3ef90249
Cholis19 <90263441+Cholis19@users.noreply.github.com>
2023-02-24 23:45:42 +0700

    Update prioritas1_if_else.dart

--- 7_branching-looping-function/praktikum/prioritas1_if_else.dart
+++ 7_branching-looping-function/praktikum/prioritas1_if_else.dart
@@ -7,9 +7,9 @@ void main() {
   //if statement
   if (nilai > 70) {
     print('nilai A');
-  } else if (nilai > 40 && nilai < 70) {
+  } else if (nilai > 40 && nilai <= 70) {
     print('nilai B');
-  } else if (nilai > 0 && nilai < 40) {
+  } else if (nilai > 0 && nilai <= 40) {
     print('nilai c');
   } else {
     print('');
