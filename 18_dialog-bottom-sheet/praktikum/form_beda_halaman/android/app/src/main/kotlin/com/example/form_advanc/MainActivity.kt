05f8439cfe1a4bb09b9cf8c69dccca22615df824
Cholis19 <cholissanctis10@gmail.com>
2023-04-02 05:17:22 +0700

    first commit

---
+++ 18_dialog-bottom-sheet/praktikum/form_beda_halaman/android/app/src/main/kotlin/com/example/form_advanc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.form_advanc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 19_flutter-navigation/praktikum/form_beda_halaman/lib/update.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FormUpdate extends StatefulWidget {
+  const FormUpdate({super.key});
+
+  @override
+  State<FormUpdate> createState() => _FormUpdateState();
+}
+
+class _FormUpdateState extends State<FormUpdate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Update Contact'),
+        ),
+        body: SafeArea(
+            child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(children: [
+                  SizedBox(height: 20),
+                  Icon(Icons.phone_android),
+                  Text(
+                    'Create New Contacts',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Membuat validasi. Untuk mengambil data yang diketika gunakan value. Di dalam validator ini kalian bebas membuat validasi sesuai dengan selera. Bisa untuk validasi email, numeric, password, dll.',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                      decoration: new InputDecoration(
+                    hintText: "masukan nama ",
+                    labelText: "Nama Kontak",
+                    icon: Icon(Icons.people),
+                    border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(5.0),
+                    ),
+                  )),
+                  TextFormField(
+                    keyboardType: TextInputType.phone,
+                    decoration: new InputDecoration(
+                      hintText: "contoh: 0812xxxxxxx",
+                      labelText: "Nomor Telp",
+                      icon: Icon(Icons.phone),
+                      border: OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(5.0)),
+                    ),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('Update'))
+                ]))));
+  }
+}
